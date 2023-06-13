72073cd999ce11813b0667da94f2f9c4e7afc3d4
Alejandro Valencia <alejandro.d.valencia@gmail.com>
2023-06-13 19:30:06 -0300

    added read me + swagger API doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('ETH wallet analitycs')
+    .setDescription('The ETH wallet analitycs API description')
+    .setVersion('1.0')
+    .addTag('eth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: [process.env.REACT_APP_API_BASE_URL],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
 bootstrap();
