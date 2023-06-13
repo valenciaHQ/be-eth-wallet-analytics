5b38eb050ce09353624921dc25bc24e1f3d6be2c
Alejandro Valencia <alejandro.d.valencia@gmail.com>
2023-06-13 19:54:05 -0300

    CORS options

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors({
-    origin: process.env.REACT_APP_API_BASE_URL,
-    methods: ['GET', 'POST'],
-  });
+  app.enableCors();
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
 bootstrap();
